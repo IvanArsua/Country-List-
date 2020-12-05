02ee835f3031d9d858c43180173c16c99e92d9e5
Ivan Joshua Punzalan Arsua <iparsua@csmbataan.ph.education>
2020-12-04 23:07:48 -0800

    Creating a file for Country list application and added a List box

---
+++ Country List/Country List/Countries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country_List
+{
+    public class Countries
+    {
+        public List<string> CountryName { get; set; }
+        public List<string> CapitalName { get; set; }
+            
+    }
+}
---
+++ Country List/Country List/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace Country_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // not working yet problem with deserializeObject 
+            var client = new RestClient("http://techslides.com/demos/country-capitals.json");
+            var request = new RestRequest("", Method.GET);
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            var data  = JsonConvert.DeserializeObject<List<Countries>>(content);
+
+            // only showing the numbers on the List 
+            List<string> countries = new List<string> () { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", };
+            List<string> names = new List<string>();
+
+            int ctr = 0;
+            foreach (string countryname in countries)
+            {
+                names.Add(ctr + " " + countries);
+                ctr = ctr + 1;
+            }
+            lstCountries.ItemsSource = names;
+            lstCountries.ItemsSource = data;
+          
+        }   
+
+        
+    }
+}
